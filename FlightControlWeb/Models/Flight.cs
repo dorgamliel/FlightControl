98e37fe395c2abc7bd9ec38506f30d72e13908e5
dorgamliel <47919011+dorgamliel@users.noreply.github.com>
2020-05-17 13:29:12 +0300

    Added Flight and FlightPlan models and controllers.

---
+++ FlightControlWeb/Models/Flight.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
+
+namespace FlightControlWeb.Models
+{
+    public class Flight
+    {
+        [Key]
+        public long Flight_ID { get; set; }
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+        public int Passengers { get; set; }
+        public string Company_Name { get; set; }
+        public DateTime Date_Time { get; set; }
+        public bool Is_External { get; set; }
+    }
+}
---
+++ FlightControlWeb/Models/FlightPlan.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace FlightControlWeb.Models
+{
+    public class FlightPlan
+    {
+        [Key]
+        [JsonIgnore]
+        public long Flight_ID { get; set; }
+        public int Passengers { get; set; }
+        public string Company_Name { get; set; }
+        public List<Segment> Segments { get; set; }
+        public Initial_Location Initial_Location { get; set; }
+    }
+    public class Segment
+    {
+        public Coordinates Coordinates { get; set; }
+        public int Timespan_Seconds { get; set; }
+    }
+    public class Initial_Location
+    {
+        public Coordinates Coordinates { get; set; }
+        public DateTime Date_Time { get; set; }
+    }
+    public class Coordinates ///check if initial and segments coordinates should be two different properties.
+    {
+        public double Longitude { get; set; }
+        public double Latitude { get; set; }
+    }
+}
