72abe5a3fb418bbeffa4168fcc9ad51a012d22b0
dorgamliel <47919011+dorgamliel@users.noreply.github.com>
2020-05-24 11:41:55 +0300

    Finished basic set up for map.

--- FlightControlWeb/wwwroot/js/Map.js
+++ FlightControlWeb/wwwroot/js/Map.js
@@ -1,36 +1,39 @@
 ﻿function initMap() {
-    var options = {
+    //Map properties.
+    let properties = {
         zoom: 13,
         center: { lat: 32.0055, lng: 34.8854 }
     }
-    //Map object
-    var map = new google.maps.Map(document.getElementById('map'), options);
+    //Map object.
+    let map = new google.maps.Map(document.getElementById('map'), properties);
     //Array of markers.
-    var markers = [
+    let markers = [
         {
             coords: { lat: 32.0055, lng: 34.8854 },
             content: 'hello<h1>world</h1>'
         }
     ];
     //Loop markers.
-    for (var i = 0; i < markers.length; i++) {
-        addMarker(markers[i]);
+    for (let i = 0; i < markers.length; i++) {
+        addMarker(markers[i], map);
     }
-    //Add Marker function.
-    function addMarker(props) {
-        var marker = new google.maps.Marker({
-            position: props.coords,
-            content: props.content,
-            map: map
+}
+
+//Add Marker function.
+function addMarker(props, map) {
+    let marker = new google.maps.Marker({
+        position: props.coords,
+        content: props.content,
+        map: map
+    });
+
+    //Adds content if exists.
+    if (props.content) {
+        let infoWindow = new google.maps.InfoWindow({
+            content: props.content
         });
-        //Adds content if exists.
-        if (props.content) {
-            var infoWindow = new google.maps.InfoWindow({
-                content: props.content
-            });
-            marker.addListener('click', function () {
-                infoWindow.open(map, marker);
-            });
-        }
+        marker.addListener('click', function () {
+            infoWindow.open(map, marker);
+        });
     }
 }
