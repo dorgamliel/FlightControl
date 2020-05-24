6ac0afda771833be5798ff2db6b39657aed3f98a
dorgamliel <47919011+dorgamliel@users.noreply.github.com>
2020-05-24 10:15:47 +0300

    Moved map script to separate js file.

---
+++ FlightControlWeb/wwwroot/js/Map.js
@@ -0,0 +1,36 @@
+﻿function initMap() {
+    var options = {
+        zoom: 13,
+        center: { lat: 32.0055, lng: 34.8854 }
+    }
+    //Map object
+    var map = new google.maps.Map(document.getElementById('map'), options);
+    //Array of markers.
+    var markers = [
+        {
+            coords: { lat: 32.0055, lng: 34.8854 },
+            content: 'hello<h1>world</h1>'
+        }
+    ];
+    //Loop markers.
+    for (var i = 0; i < markers.length; i++) {
+        addMarker(markers[i]);
+    }
+    //Add Marker function.
+    function addMarker(props) {
+        var marker = new google.maps.Marker({
+            position: props.coords,
+            content: props.content,
+            map: map
+        });
+        //Adds content if exists.
+        if (props.content) {
+            var infoWindow = new google.maps.InfoWindow({
+                content: props.content
+            });
+            marker.addListener('click', function () {
+                infoWindow.open(map, marker);
+            });
+        }
+    }
+}
